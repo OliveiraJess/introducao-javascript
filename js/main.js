b3a8efc27833bb26c9782765c87da5663619b600
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-07 16:43:30 -0300

    pegando o peso e altura do primeiro paciente para calcular o imc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-const titulo = document.querySelector("h1");
+//pega a classe titulo
+const titulo = document.querySelector(".titulo");
 
-//altera texto dentro da tag h1
+//altera texto dentro da tag com a classe titulo
 titulo.textContent = "Nutricionista";
+
+//pega o id primeiro paciente
+const paciente = document.querySelector("#primeiro-paciente");
+
+//pega peso do primeiro-paciente
+const tdPeso = paciente.querySelector(".info-peso");
+const peso = tdPeso.textContent;
+
+//pega altura do primeiro-paciente
+const tdAltura = paciente.querySelector(".info-altura");
+const altura = tdAltura.textContent;
+
+//calculo do imc 
+const imc = peso / (altura * altura); // 100 / 2.0 x 2.0 = 100 / 4 =>>>>>>> 25
+
+console.log(imc);
