e9cc94d0b77534ad08432ff3203a0601353cdd53
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-08 10:47:39 -0300

    calculando o imc de todos os pacientes

--- js/main.js
+++ js/main.js
@@ -4,39 +4,47 @@ const titulo = document.querySelector(".titulo");
 //altera texto dentro da tag com a classe titulo
 titulo.textContent = "Nutricionista";
 
-//pega o id primeiro paciente
-const paciente = document.querySelector("#primeiro-paciente");
+//pega os pacientes
+const pacientes = document.querySelectorAll(".paciente");
 
-//pega peso do primeiro-paciente
-const tdPeso = paciente.querySelector(".info-peso");
-const peso = tdPeso.textContent;
 
-//pega altura do primeiro-paciente
-const tdAltura = paciente.querySelector(".info-altura");
-const altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+    console.log(pacientes[i]);
 
-//pegando o imc e exibindo no imc da tabela
-const tdImc = paciente.querySelector(".info-imc");
+    let paciente = pacientes[i];
 
-//validacoes
-let pesoEhValido = true;
-let alturaEhValida = true;
+    //pega peso do primeiro-pacientes
+    const tdPeso = paciente.querySelector(".info-peso");
+    const peso = tdPeso.textContent;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
+    //pega altura do primeiro-paciente
+    const tdAltura = paciente.querySelector(".info-altura");
+    const altura = tdAltura.textContent;
+
+    //pegando o imc e exibindo no imc da tabela
+    const tdImc = paciente.querySelector(".info-imc");
+
+    //validacoes
+    let pesoEhValido = true;
+    let alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+    }
+
+    //calculo do imc 
+    if (alturaEhValida & pesoEhValido) {
+        const imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
 }
-
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
-
-//calculo do imc 
-if (alturaEhValida & pesoEhValido) {
-    const imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
-
