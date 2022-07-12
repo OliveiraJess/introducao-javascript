87e3b0f5e3cadda89f1feb95613b560f09b7d66c
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-12 16:16:11 -0300

    animação ao remover paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,5 +1,10 @@
 const tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick",function(event){
-    event.target.parentNode.remove();
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
+
 });
